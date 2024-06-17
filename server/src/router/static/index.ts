129b6c0636bd7c871f2243f1ccaf6b29bb096122
Joe Boylson <joeboylson@joes-mac-mini.lan>
2024-06-17 16:25:11 -0400

    debugging

--- server/src/router/static/index.ts
+++ server/src/router/static/index.ts
@@ -1,13 +1,15 @@
 import express, { Response, Express } from "express";
 import path from "path";
 
+const PUBLIC_FOLDER = "./../../../../public";
+
 export function setStaticFolder(app: Express) {
-  app.use(express.static(path.join(__dirname, "./public")));
+  app.use(express.static(path.join(__dirname, PUBLIC_FOLDER)));
   app.use((_: unknown, response: Response) => {
     const isProduction = process.env.MODE === "production";
 
     if (isProduction) {
-      const frontEndPath = path.join(__dirname, "./public/index.html");
+      const frontEndPath = path.join(__dirname, PUBLIC_FOLDER, "index.html");
       return response.sendFile(frontEndPath);
     }
 
